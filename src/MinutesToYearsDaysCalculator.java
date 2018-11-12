3496897cb5291a189326a303eced0fbf993c15df
Niraj Dubey <nirajd24@gmail.com>
2018-11-13 02:03:02 +0530

    New Programs

---
+++ src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,20 @@
+public class MinutesToYearsDaysCalculator {
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    private static void printYearsAndDays(long minutes){
+        if(minutes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            long hours = minutes / 60;
+            long days = hours / 24;
+            long years = days / 365;
+            days = days % 365;
+
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
