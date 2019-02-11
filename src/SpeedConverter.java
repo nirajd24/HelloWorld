35514869991dec6147471e9e977cacee522301ac
Niraj Dubey <nirajd24@gmail.com>
2019-02-12 00:01:15 +0530

    New Programs

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,13 +1,6 @@
 public class SpeedConverter {
 
     public static void main(String[] args) {
-        System.out.println();
-        toMilesPerHour(1.5);
-        toMilesPerHour(10.25);
-        toMilesPerHour(-5.6);
-        toMilesPerHour(25.42);
-        toMilesPerHour(75.114);
-
         printConversion(1.5);
         printConversion(10.25);
         printConversion(-5.6);
@@ -15,12 +8,12 @@ public class SpeedConverter {
         printConversion(75.114);
     }
 
-    public static long toMilesPerHour(double kilometersPerHour) {
+    private static long toMilesPerHour(double kilometersPerHour) {
         if(kilometersPerHour < 0) return -1;
         return Math.round(kilometersPerHour / 1.609);
     }
 
-    public static void printConversion(double kilometersPerHour) {
+    private static void printConversion(double kilometersPerHour) {
         if(kilometersPerHour < 0) System.out.println("Invalid Value");
         else {
             long milesPerHour = toMilesPerHour(kilometersPerHour);
