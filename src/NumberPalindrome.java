9acf9116ed67c44c784d350a7a00c18912f70aa5
Niraj Dubey <nirajd24@gmail.com>
2019-04-27 20:49:15 +0530

    New Programs

--- src/NumberPalindrome.java
+++ src/NumberPalindrome.java
@@ -1,6 +1,6 @@
 public class NumberPalindrome {
     public static void main(String[] args){
-        System.out.println(isPalindrome(1221));
+        System.out.println(isPalindrome(-222));
     }
 
     public static boolean isPalindrome(int number){
@@ -8,12 +8,14 @@ public class NumberPalindrome {
         int lastDigit = 0;
         int original = number;
 
-        while(number > 0){
-            lastDigit = number % 10;//1 2 2 1
-            reverse = reverse * 10;//0 10 120 1220
-            reverse = reverse + lastDigit;//1 12 122 1221
-            number = number / 10;//122 12 1 0
+        while(number != 0){
+            lastDigit = number % 10;//1 2 2 1   -2 -2 -2
+            reverse = reverse * 10;//0 10 120 1220  0 -20 -220
+            reverse = reverse + lastDigit;//1 12 122 1221   -2 -22 -222
+            number = number / 10;//122 12 1 0   -22 -2 0
         }
+
+        //System.out.println(original + " == " + reverse);
 
         if(original == reverse)
             return true;
