15b6cb014a60acb852e14efe44a00acf7c6e8cf4
Niraj Dubey <nirajd24@gmail.com>
2019-05-10 00:03:51 +0530

    New Programs

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,11 +1,30 @@
 public class GreatestCommonDivisor {
 
     public static void main(String[] args){
-        System.out.println(getGreatestCommonDivisor(41, 22));
+        System.out.println(getGreatestCommonDivisor(12, 30));
     }
 
     public static int getGreatestCommonDivisor(int first, int second){
+        if(first < 10 || second < 10){
+            return -1;
+        }
 
-        return -1;
+        int i = 1;
+        int gcd = 1;
+
+        while(gcd <= i){
+
+            int fLastDigit = first % i;
+            int sLastDigit = second % i;
+
+            if((fLastDigit == 0) && (sLastDigit == 0)){
+                gcd = i;
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        return gcd;
     }
 }
